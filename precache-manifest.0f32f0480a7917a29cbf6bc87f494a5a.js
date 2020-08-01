9f9c519f7a3957e49c5189b366d5bf9def19cb08
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2020-08-01 05:23:53 -0500

    Updates

---
+++ precache-manifest.0f32f0480a7917a29cbf6bc87f494a5a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad364585b33fc90cb1b6ab4f0c373af",
+    "url": "/react-parallax-vercel/index.html"
+  },
+  {
+    "revision": "f4dff95631ea549ba40b",
+    "url": "/react-parallax-vercel/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ffa3c45d577d711dba56",
+    "url": "/react-parallax-vercel/static/css/main.10e5e0d9.chunk.css"
+  },
+  {
+    "revision": "f4dff95631ea549ba40b",
+    "url": "/react-parallax-vercel/static/js/2.292ac8d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-parallax-vercel/static/js/2.292ac8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa3c45d577d711dba56",
+    "url": "/react-parallax-vercel/static/js/main.832fbc98.chunk.js"
+  },
+  {
+    "revision": "fb316d8d224ffe4241c9",
+    "url": "/react-parallax-vercel/static/js/runtime-main.b310b4dc.js"
+  },
+  {
+    "revision": "8e43a7a4b66e0877ab06fb9b1fc3be42",
+    "url": "/react-parallax-vercel/static/media/bg-1.8e43a7a4.png"
+  },
+  {
+    "revision": "23be9423712efc058eff6cdf71312dde",
+    "url": "/react-parallax-vercel/static/media/carousel-1.23be9423.png"
+  },
+  {
+    "revision": "1a44c97514c115c5ea36df173a9f2184",
+    "url": "/react-parallax-vercel/static/media/carousel-2.1a44c975.jpg"
+  },
+  {
+    "revision": "0686d44d311f49e900b6a95ab1b4e638",
+    "url": "/react-parallax-vercel/static/media/carousel-3.0686d44d.png"
+  },
+  {
+    "revision": "89b0fc52130c18f26763b847c39c5faf",
+    "url": "/react-parallax-vercel/static/media/logo-kodemia.89b0fc52.png"
+  },
+  {
+    "revision": "9583c1e083dc58b7e16ea09616e9d89e",
+    "url": "/react-parallax-vercel/static/media/preview-messebau-virtual.9583c1e0.png"
+  },
+  {
+    "revision": "4b55689df38f759a53cfd4f28a74f1fa",
+    "url": "/react-parallax-vercel/static/media/preview-messebau.4b55689d.png"
+  },
+  {
+    "revision": "26bd0e6a2c9d196b4e172e08cba74ead",
+    "url": "/react-parallax-vercel/static/media/preview-skillup.26bd0e6a.png"
+  }
+]);
